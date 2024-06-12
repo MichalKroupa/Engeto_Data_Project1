406b581a9f5777bdeaab31dbd4e6d7db6db3aff4
MichalKroupa <95313057+MichalKroupa@users.noreply.github.com>
2024-06-12 10:05:46 +0200

    Update Sekundarni_Tabulka.sql

--- Sekundarni_Tabulka.sql
+++ Sekundarni_Tabulka.sql
@@ -9,7 +9,7 @@ CREATE TABLE t_michal_kroupa_project_SQL_secondary_final AS(
 	JOIN t_michal_kroupa_project_sql_primary_final tmk 
 		ON tmk.`year` = e.`year`
 	WHERE
-		e.country in ('Czech Republic', 'Slovakia', 'Germany', 'Austria', 'Poland')
+		e.country IN ('Czech Republic', 'Slovakia', 'Germany', 'Austria', 'Poland')
 		AND e.GDP IS NOT NULL
 );
 
