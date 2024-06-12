b8b9bb51a25aa18c4bec79aea5b9e016394760b6
MichalKroupa <95313057+MichalKroupa@users.noreply.github.com>
2024-06-12 10:06:12 +0200

    Update Druha_Otazka.sql

--- Druha_Otazka.sql
+++ Druha_Otazka.sql
@@ -3,7 +3,7 @@
 	product_name,
 	AVG(industry_payroll) AS payroll_avg,
 	AVG(product_price) AS price_avg,
-	ROUND(AVG(industry_payroll) / AVG(product_price), 0)  AS can_buy
+	ROUND(AVG(industry_payroll) / AVG(product_price), 0) AS can_buy
 FROM t_michal_kroupa_project_sql_primary_final tmkpspf
 WHERE 
 	product_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
